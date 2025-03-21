955f8263dab6e8fbfd28455e5292f1a4b2c70839
chaotic-justice <deepocean.2900@gmail.com>
2025-03-21 17:16:35 -0400

    extra period

--- src/app/(frontend)/vars/page.tsx
+++ src/app/(frontend)/vars/page.tsx
@@ -1,5 +1,5 @@
 export default async function Page() {
   const letter = process.env.NEXT_PUBLIC_SERVER_URL
 
-  return <div>{letter} should be sealed.</div>
+  return <div>{letter} should be sealed.?</div>
 }
