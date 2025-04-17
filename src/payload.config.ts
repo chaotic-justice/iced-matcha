7bc5a84970d40e5d27c28197841d66d27ce6af8d
chaotic-justice <deepocean.2900@gmail.com>
2025-04-17 14:45:25 -0400

    Increase Timeout Limit

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -68,7 +68,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
-    push: false,
+    push: true,
   }),
   collections: [Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
