fca8ded5e29d4a66b83b9f595b422ce386a0aa04
chaotic-justice <deepocean.2900@gmail.com>
2025-04-17 21:46:34 -0400

    push false

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -68,7 +68,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
-    push: true,
+    push: false,
   }),
   collections: [Pages, Posts, Media, Categories, Users],
   cors: [getServerSideURL()].filter(Boolean),
